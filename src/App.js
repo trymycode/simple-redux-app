3b17adabe86035efa886829e20ed934d393542b7
Imita Singha <imita@psqad.com>
2019-07-19 18:00:30 +0530

    added redux, react-redux, redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React , {Component} from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore , applyMiddleware } from 'redux';
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
-function App() {
-  return (
-    <div className="App">
-      <PostForm />
-      <Posts />
-    </div>
-  );
+const store = createStore(()=>[], {}, applyMiddleware());
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+ 
 }
 
 export default App;
